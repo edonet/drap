23c87b1fb76088c6daeecf409d30a3b8e7b70ed7
edonet <edonet@163.com>
2019-02-14 13:38:48 +0800

    feat: add lib

--- bin/drap.js
+++ bin/drap.js
@@ -15,44 +15,7 @@
  * 加载依赖
  *****************************************
  */
-const yargs = require('yargs');
-const fs = require('airk/fs');
-const pkg = require('../package.json');
-const copy = require('./copy');
-
-
-/**
- *****************************************
- * 定义参数
- *****************************************
- */
-yargs
-    .boolean('copy')
-    .alias('c', 'copy')
-    .alias('t', 'target');
-
-
-/**
- *****************************************
- * 定义脚本
- *****************************************
- */
-async function run() {
-    let argv = yargs.argv,
-        src = argv.src || argv._[0] || pkg.library || 'lib',
-        dist = argv.dist || argv._[1] || 'dist';
-
-    // 判断是否存在源文件
-    if (await fs.stat(src)) {
-
-        // 创建文件目录
-        await fs.rmdir(dist);
-        await fs.mkdir(dist);
-
-        // 复制文件
-        await copy(src, dist, argv);
-    }
-}
+const drap = require('../lib/drap');
 
 
 /**
@@ -60,4 +23,4 @@ async function run() {
  * 启动脚本
  *****************************************
  */
-module.exports = run().catch(console.error);
+module.exports = drap().catch(console.error);
